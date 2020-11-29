6bd976ec4232b5e46f5051ee67827d4afba55697
yogesh kanjal <kanjal.ysk123@gmail.com>
2020-11-29 23:24:02 +0530

    removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side java script file is loaded')
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg1')
