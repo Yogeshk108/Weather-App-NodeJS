67cb59f495d4bc9783caee34dcb6479b96248369
yogesh kanjal <kanjal.ysk123@gmail.com>
2020-11-30 01:26:23 +0530

    Add wind speed and UV index

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('postman-request')
 
+
+
 const forecast= (longitude , latitude , callback) => {
     const forecasturl = 'http://api.weatherstack.com/current?access_key=082ac69fdc63935998b59fa82ff73c35&query=' + latitude + ',' + longitude + '&units=m'
     request({url:forecasturl , json:true} , (error , {body}={}) => {
@@ -10,7 +12,7 @@ const forecast= (longitude , latitude , callback) => {
         } else if (body.error) {
             callback('unable to find location' , undefined)
         } else {
-            callback(undefined , 'it is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees')
+            callback(undefined , body.current.weather_descriptions[0] +'.It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees.' + 'UV index is ' + body.current.uv_index + '.Wind speed is ' + body.current.wind_speed + ' KMPH and wind direction is ' + body.current.wind_dir + '.')
         }
     })
 }
